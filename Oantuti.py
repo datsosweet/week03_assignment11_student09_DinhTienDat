02d8fb5324fbf91c5b2a1519fb2fe3d439087d6f
datsosweet <109160530+datsosweet@users.noreply.github.com>
2022-07-30 20:57:29 +0700

    Add files via upload

---
+++ Oantuti.py
@@ -0,0 +1,43 @@
+from random import randint
+
+print('Nhap Dam, La hoac Keo')
+
+player =input()
+computer = randint(0,2)
+
+if computer == 0:
+	computer = "Dam"
+if computer == 1:
+	computer = "La"
+if computer == 2:
+	computer = "Keo"
+
+print('---')
+print('You choose: ' + player)
+print('computer chooses: ' + computer)
+print('---')
+
+if player == computer:
+	print('Draw')
+
+else:
+	if player == "Keo":
+		if computer == "Dam":
+			print('Lose')
+		else:
+			print(Win)
+
+	elif player == "Dam":
+		if computer == "Keo":
+			print ('Win')
+		else:
+			print(Lose)
+
+	elif player == "La":
+		if computer == "Keo":
+			print ('Lose')
+		else:
+			print('Win')
+
+	else:
+		print('Nhap khong dung')
